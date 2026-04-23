a1317f69575eaa26356d82ed68fecab82709dbcd
F4R105 <kajubufaraji@gmail.com>
2026-04-23 19:11:27 +0300

    create order and order items models, migrations and views

---
+++ database/migrations/2026_04_23_151408_create_order_tables.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Order;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('price');
+            $table->string('created_by');
+            $table->string('served_by');
+            $table->timestamps();
+        });
+
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('unit_price');
+            $table->integer('quantity')->default(1);
+            $table->foreignIdFor(Order::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_items');
+    }
+};
