47b31609c97498e00be8c8fd322ec9d8dde4a8ef
F4R105 <kajubufaraji@gmail.com>
2026-04-27 11:02:09 +0300

    add hardcoded dashboard quick values

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,23 @@
 <x-auth-layout title="Dashboard">
     <h1>Welcome to the dashboard</h1>
+
+    {{-- items --}}
+    <ul class="list-disc list-inside">
+        <li>Items: 6</li>
+        <li>Most sold: Chapati</li>
+    </ul>
+
+    {{-- users --}}
+    <ul class="list-disc list-inside">
+        <li>Admins: 3</li>
+        <li>Employees: 5</li>
+    </ul>
+
+    {{-- orders (daily) --}}
+    <ul class="list-disc list-inside">
+        <li>Total orders: 9</li>
+        <li>Served orders: 4</li>
+        <li>Pending orders: 5</li>
+        <li>Sales: TZS 5000/=</li>
+    </ul>
 </x-auth-layout>
