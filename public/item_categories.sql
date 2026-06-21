035aa0429c6dc9839f8b69ee8ed39f147cfb0eeb
Faraji <kajubufaraji@gmail.com>
2026-06-21 22:38:16 +0300

    add sql files on public folder

---
+++ public/item_categories.sql
@@ -0,0 +1,8 @@
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:24:19', '2', 1, 'Hot beverage', '2026-06-21 21:24:19');
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:32:06', '3', 1, 'Soup', '2026-06-21 21:32:06');
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:32:16', '4', 1, 'Starter', '2026-06-21 21:32:16');
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:32:30', '5', 1, 'Fresh pastry', '2026-06-21 21:32:30');
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:32:38', '6', 1, 'Chicken', '2026-06-21 21:32:38');
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:32:47', '7', 1, 'Beef', '2026-06-21 21:32:47');
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:32:55', '8', 1, 'Fish', '2026-06-21 21:32:55');
+insert into `item_categories` (`created_at`, `id`, `is_active`, `name`, `updated_at`) values ('2026-06-21 21:33:04', '9', 1, 'Goat', '2026-06-21 21:33:04');
---
+++ public/items.sql
@@ -0,0 +1,59 @@
+INSERT INTO items (name, unit_price, category_id, created_at, updated_at) VALUES
+-- HOT BEVERAGE (category_id: 2)
+('Black Tea (Chai ya Rangi)', 500, 2, NOW(), NOW()),
+('White Tea (Chai ya Maziwa)', 1000, 2, NOW(), NOW()),
+('Coffee (Kahawa)', 2000, 2, NOW(), NOW()),
+('Fresh Milk', 1000, 2, NOW(), NOW()),
+
+-- SOUP (category_id: 3)
+('Chicken Soup (Supu ya Kuku)', 8000, 3, NOW(), NOW()),
+('Beef Soup (Supu ya Ng''ombe)', 4000, 3, NOW(), NOW()),
+('Goat Soup (Supu ya Mbuzi)', 4000, 3, NOW(), NOW()),
+('Fish Soup (Supu ya Samaki)', 10000, 3, NOW(), NOW()),
+('Boiled Beef (Mchemsho wa Ngo''mbe)', 5000, 3, NOW(), NOW()),
+('Boiled Fish (Mchemsho wa Samaki)', 13000, 3, NOW(), NOW()),
+
+-- STARTER (category_id: 4)
+('Chips Mayai', 3000, 4, NOW(), NOW()),
+('Plain Chips', 2000, 4, NOW(), NOW()),
+('Beef Samosa', 500, 4, NOW(), NOW()),
+('PANCAKE (Chapati za Maji)', 500, 4, NOW(), NOW()),
+('Eggs Chop', 2000, 4, NOW(), NOW()),
+('Sausage (Big)', 2000, 4, NOW(), NOW()),
+('Sausage (Small)', 1000, 4, NOW(), NOW()),
+
+-- FRESH PASTRY (category_id: 5)
+('Chapati', 500, 5, NOW(), NOW()),
+('Maandazi', 500, 5, NOW(), NOW()),
+('Half Cake / Sconze', 500, 5, NOW(), NOW()),
+('Cripsy (Kaukau)', 500, 5, NOW(), NOW()),
+('Vitumbua or Donati', 500, 5, NOW(), NOW()),
+('Brown bread - 2pc', 1000, 5, NOW(), NOW()),
+('White Bread - 2pc', 1000, 5, NOW(), NOW()),
+
+-- CHICKEN (category_id: 6)
+('Enea Mix Food', 25000, 6, NOW(), NOW()),
+('Chicken Biryan', 12000, 6, NOW(), NOW()),
+('Chicken Ugali or Rice', 10000, 6, NOW(), NOW()),
+('Grilled Chicken (kuku choma)', 8000, 6, NOW(), NOW()),
+('Stir fry Chicken (kuku ya Kukaanga)', 8000, 6, NOW(), NOW()),
+
+-- BEEF (category_id: 7)
+('Beef Biryan', 10000, 7, NOW(), NOW()),
+('Beef Roast with Ugali or Rice', 4000, 7, NOW(), NOW()),
+('Beef Pilau Roast', 5000, 7, NOW(), NOW()),
+('Beef Curry', 8000, 7, NOW(), NOW()),
+('Grilled Beef', 5000, 7, NOW(), NOW()),
+('Beef and Banana Stew (Ndizi Nyama)', 5000, 7, NOW(), NOW()),
+('Beef Makange', 8000, 7, NOW(), NOW()),
+
+-- FISH (category_id: 8)
+('Fried Fish with Ugali or Rice', 8000, 8, NOW(), NOW()),
+('Fish Makange with Ugali or Rice', 4000, 8, NOW(), NOW()),
+('Fish Coconut Sauce Served with Ugali or Rice', 15000, 8, NOW(), NOW()),
+
+-- GOAT (category_id: 9)
+('Goat Rost & Ugali or Rice (Mbuzi Kukaanga)', 4000, 9, NOW(), NOW()),
+('Goat Biryan (Mbuzi Biryan)', 10000, 9, NOW(), NOW()),
+('Grilled Goat (Mbuzi Choma)', 6000, 9, NOW(), NOW()),
+('Goat Pilau (Mbuzi Pilau)', 6000, 9, NOW(), NOW());
